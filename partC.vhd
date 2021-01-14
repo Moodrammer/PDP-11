b7e21165e04e6d187914475ef7368b8c9f608495
hossamalaa69 <hossamalaa69.ha@gmail.com>
2021-01-14 19:25:35 +0200

    Modify PartC in ALU

--- partC.vhd
+++ partC.vhd
@@ -4,8 +4,8 @@ use ieee.std_logic_1164.all;
 entity partC is 
         GENERIC (n : integer := 16);
 	port(
-	S1, S0, CIN : in std_logic;
-	A : in std_logic_vector (n-1 downto 0);
+	S1, S0 : in std_logic;
+	A, B : in std_logic_vector (n-1 downto 0);
 	F : out std_logic_vector (n-1 downto 0);
 	COUT : out std_logic);
 end entity;
@@ -13,11 +13,12 @@ end entity;
 Architecture archC of partC is
 begin
 
-	COUT <= A(0);
+	COUT <= '0' when (s1='1' and s0='0')
+	else B(0);
 
-	F <= ('0' & A(n-1 downto 1))  when (s1='0' and s0='0')
-	else (A(0) & A(n-1 downto 1)) when (s1='0' and s0='1')
-	else (CIN & A(n-1 downto 1)) when (s1='1' and s0='0')
-	else (A(n-1) & A(n-1 downto 1));
+	F <= ('0' & B(n-1 downto 1))  when (s1='0' and s0='0')
+	else (B(0) & B(n-1 downto 1)) when (s1='0' and s0='1')
+	else (A xor B) when (s1='1' and s0='0')
+	else (B(n-1) & B(n-1 downto 1));
 
 end Architecture;
