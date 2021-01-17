ac223e0e1cacd96fba055e052ca63cc590956007
Muhammad Ahmad Hesham <muhammad.mahmoud99@eng-st.cu.edu.eg>
2021-01-18 01:35:54 +0200

    add remaining registers

--- ALU.vhd
+++ ALU.vhd
@@ -1,5 +1,7 @@
-Library ieee;
+library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
 entity ALU is 
 GENERIC (n : integer := 16);  
@@ -7,8 +9,8 @@ GENERIC (n : integer := 16);
   	  A, B:  in std_logic_vector (n-1 downto 0);
   	  S:     in std_logic_vector (3 downto 0);
   	  CIN:   in std_logic;
-	  F:     out std_logic_vector (n-1 downto 0);
-  	  COUT:  out std_logic);
+	  	F:     out std_logic_vector (n-1 downto 0);
+  	  COUT, zeroFlag, negativeFlag:  out std_logic);
 end entity;
 
 Architecture archALU of ALU is
@@ -49,7 +51,7 @@ Architecture archALU of ALU is
 	COUT : out std_logic);
    end component;
    
-   signal FA, FB, FC, FD: std_logic_vector (n-1 downto 0);
+   signal FA, FB, FC, FD, tempF: std_logic_vector (n-1 downto 0);
    signal coutA, coutB, coutC, coutD: std_logic;
 
 begin
@@ -59,14 +61,20 @@ begin
 	uc: partC generic map(n) port map (S(1), S(0), A, B, FC, coutC);
 	ud: partD generic map(n) port map (S(1), S(0), A, B, FD, coutD);
 
-	F <= FA when (S(3 downto 2)="00")
+	tempF <= FA when (S(3 downto 2)="00")
 	else FB when (S(3 downto 2)="01")
 	else FC when (S(3 downto 2)="10")
 	else FD when (S(3 downto 2)="11");
+
+	F <= tempF;
 
 	COUT <= coutA when (S(3 downto 2)="00")
 	else    coutB when (S(3 downto 2)="01")
 	else	coutC when (S(3 downto 2)="10")
 	else    coutD when (S(3 downto 2)="11");
+
+	negativeFlag <= tempF(n-1);
+	zeroFlag <= '1' when unsigned(tempF) = 0
+	else '0';
 
 end archALU;
--- PDP11.vhd
+++ PDP11.vhd
@@ -4,41 +4,60 @@ use ieee.std_logic_1164.all;
 entity PDP11 is
   GENERIC (n : integer := 16);    
 	port(
-    clk: in std_logic;
-    reset: in std_logic
-    );
+      clk: in std_logic;
+      reset: in std_logic
+  );
 end entity;
 
 architecture arch of PDP11 is
   
   -- Register
   component Register_nbits IS
-        GENERIC (n : integer := 16);
-	PORT( Clk,Rst,en : IN std_logic;
-		       d : IN std_logic_vector(n-1 DOWNTO 0);
-		       q : OUT std_logic_vector(n-1 DOWNTO 0));
+    GENERIC (n : integer := 16);
+    PORT( 
+        Clk,Rst,en : IN std_logic;
+        d : IN std_logic_vector(n-1 DOWNTO 0);
+        q : OUT std_logic_vector(n-1 DOWNTO 0)
+    );
   END component;
 
   component counter is
-    generic (n: integer);
+    GENERIC (n : integer := 16);
     port (
       clk, rst, loadEnable: in std_logic;
       load: in std_logic_vector(n-1 downto 0);
       count: out std_logic_vector(n-1 downto 0);
       countEnable: in std_logic
-    ) ;
+    );
   end component;
 
   -- Tristate
   component Tristate IS
-	GENERIC (n : integer := 16);
-	PORT(	          en : IN std_logic;
-      		input_signal : IN std_logic_vector(n-1 DOWNTO 0);
-	       output_signal : OUT std_logic_vector(n-1 DOWNTO 0));
+    GENERIC (n : integer := 16);
+    PORT(
+      en : IN std_logic;
+      input_signal : IN std_logic_vector(n-1 DOWNTO 0);
+      output_signal : OUT std_logic_vector(n-1 DOWNTO 0)
+    );
   END component;
 
+  -- ALU
+  component ALU is
+    GENERIC (n : integer := 16);  
+    port(
+      A, B:  in std_logic_vector (n-1 downto 0);
+      S:     in std_logic_vector (3 downto 0);
+      CIN:   in std_logic;
+      F:     out std_logic_vector (n-1 downto 0);
+      COUT, zeroFlag, negativeFlag:  out std_logic
+    );
+  end component;
+
+
   -- register
-  signal internal_bus,register0, register1, register2, register3, register4, register5, register6, register7, mdregister, maregister, ir: std_logic_vector(15 downto 0);
+  signal internal_bus,register0, register1, register2, register3, register4, register5, register6, register7: std_logic_vector(15 downto 0);
+  signal mdregister, maregister, iregister, iregister_temp, iregister_address, tempregister, Yregister, Zregister: std_logic_vector(15 downto 0);
+  signal flagregister: std_logic_vector(15 downto 0);
 
   -- micro ar
   signal microAr: std_logic_vector(7 downto 0);
@@ -48,12 +67,21 @@ architecture arch of PDP11 is
 
   -- registers in enable
   signal r0_in, r1_in, r2_in, r3_in, r4_in, r5_in, r6_in, r7_in, pc_in, mdr_in, mar_in: std_logic; 
-  signal r7_in_en, pc_inc: std_logic; 
+  signal r7_in_en, pc_inc: std_logic;
+  signal ir_in, temp_in, Y_in, Z_in: std_logic;
 
   -- registers out enable
   signal r0_out, r1_out, r2_out, r3_out, r4_out, r5_out, r6_out, r7_out, pc_out, mdr_out, mar_out: std_logic;
   signal r7_out_en: std_logic;
+  signal ir_address_out, temp_out, Z_out: std_logic;
 
+  -- ALU
+  signal ALU_output: std_logic_vector(15 downto 0);
+  signal ALU_B_input, ALU_A_input, ALU_Flag_in: std_logic_vector(15 downto 0);
+  signal ALU_carryFlag, ALU_zeroFlag, ALU_negativeFlag: std_logic;
+
+  -- control store
+  signal F6: std_logic_vector(3 downto 0); -- ALU group
 begin
 
   ---------------------------------------------------------------------------------------------------
@@ -87,5 +115,37 @@ begin
 
   mdr: Register_nbits generic map(n) port map (clk, '0', mdr_in, internal_bus, mdregister);
   mar: Register_nbits generic map(n) port map (clk, '0', mar_in, internal_bus, maregister);
+
+  ----------------------------------------------------------------------------------------------
+  -- instruction register
+
+  tri_ir_address: Tristate generic map(n) port map(ir_address_out, iregister_address, internal_bus);
+
+  ireg: Register_nbits generic map(n) port map (clk, '0', ir_in, internal_bus, iregister);
+  iregister_temp <= "00000000" & iregister(7 downto 0);
+  ireg_address: Register_nbits generic map(n) port map (clk, '0', '1', iregister_temp, iregister_address);
+
+
+  ----------------------------------------------------------------------------------------------
+  -- ALU
+  ALU_A_input <= internal_bus;
+  ALU_B_input <= Yregister;
+  alunit: ALU generic map(n) port map(ALU_A_input, ALU_B_input, F6, carryFlag, ALU_output, ALU_carryFlag, ALU_zeroFlag, ALU_negativeFlag);
+
+  ALU_Flag_in <= "0000000000000" & ALU_carryFlag & ALU_zeroFlag & ALU_negativeFlag;
+  flagreg: Register_nbits generic map(n) port map (clk, '0', Z_in, ALU_Flag_in, flagregister);
+  carryFlag <= flagregister(2);
+  zeroFlag <= flagregister(1); 
+  negativeFlag <= flagregister(0); 
+
+  ----------------------------------------------------------------------------------------------
+  -- transparent registers
+
+  tri_Z: Tristate generic map(n) port map(Z_out, Zregister, internal_bus);
+  tri_temp: Tristate generic map(n) port map(temp_out, tempregister, internal_bus);
+
+  tempreg: Register_nbits generic map(n) port map (clk, '0', temp_in, internal_bus, tempregister);
+  Zreg: Register_nbits generic map(n) port map (clk, '0', Z_in, ALU_output, Zregister);
+  Yreg: Register_nbits generic map(n) port map (clk, '0', Y_in, internal_bus, Yregister);
 
 end arch ; -- arch
